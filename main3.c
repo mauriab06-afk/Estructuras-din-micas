1398f6ecab9d631c67a61a795771c56fa554e400
mauriab06-afk <mauriab06@gmail.com>
2025-12-17 04:00:34 +0000

    ordemnamiento por bublee

--- main3.c
+++ main3.c
@@ -6,16 +6,42 @@ typedef struct nodo{
     struct nodo* sig;
 }NODO;
 
+void imprimirLista(NODO* cabeza);
 int buscarPosicion(NODO* cabeza, int num);
 NODO* leerArchivo(int* numElementos);
+void ordenarListaMayor(NODO* cabeza);
 
 int main(void){
     int numElementos;
     int num, posicion;
     NODO* cabeza = leerArchivo(&numElementos);
+    /*
     fprintf(stdout,"Ingrese el numero a buscar su posicion: ");
     fscanf(stdin,"%d",&num);
     posicion = buscarPosicion(cabeza, num);
+    */
+   imprimirLista(cabeza);
+   ordenarListaMayor(cabeza);
+   fprintf(stdout,"Se ordeno\n");
+   imprimirLista(cabeza);
+}
+
+void ordenarListaMayor(NODO* cabeza){ //Ordenar de mayor a menor
+    NODO* posicion = cabeza; //La posicion
+    NODO* recorrido; //El recorrido
+    int temporal; //Guardara temporalmente un numero
+    while(posicion != NULL){
+            recorrido = posicion->sig;
+        while(recorrido != NULL){
+            if(posicion->num < recorrido->num){                    
+                temporal = posicion->num;
+                posicion->num = recorrido->num;
+                recorrido->num = temporal;
+            }
+            recorrido = recorrido->sig;
+        }   
+        posicion = posicion->sig;
+    }
 }
 
 int buscarPosicion(NODO* cabeza, int num){
@@ -30,6 +56,13 @@ int buscarPosicion(NODO* cabeza, int num){
         return posicion;
     }
     
+}
+
+void imprimirLista(NODO* cabeza){
+    while(cabeza != NULL){
+        fprintf(stdout,"%d\n",cabeza->num);
+        cabeza = cabeza->sig; //Recorre e imprime
+    }
 }
 
 NODO* leerArchivo(int* numElementos){
